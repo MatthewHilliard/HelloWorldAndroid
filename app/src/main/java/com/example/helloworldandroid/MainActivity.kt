63e2384f369ff8f3666934449cabe14aa2a1cd98
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-01-22 19:51:45 -0500

    Added working hello world button

--- app/src/main/java/com/example/helloworldandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloworldandroid/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.helloworldandroid
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.buttonHelloWorld)
+        button.setOnClickListener(){
+            val sayHelloWorld = findViewById<TextView>(R.id.helloWorldText)
+            sayHelloWorld.text = "Hello World!"
+        }
     }
 }
